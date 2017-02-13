3355e9ddf70351b9c7b0a0f737e5eb030d6997d1
A13XIS <allseeingstarlp@gmail.com>
2017-02-14 00:09:07 +0100

    Fix manual update errors again

--- src/main/java/inc/a13xis/legacy/koresample/common/util/event/ForgeEventListener.java
+++ src/main/java/inc/a13xis/legacy/koresample/common/util/event/ForgeEventListener.java
@@ -1,6 +1,7 @@
 package inc.a13xis.legacy.koresample.common.util.event;
 
 import net.minecraftforge.fml.common.eventhandler.EventBus;
+import net.minecraftforge.event.ForgeEventFactory;
 
 @SuppressWarnings({ "AbstractClassNeverImplemented", "WeakerAccess" })
 public abstract class ForgeEventListener
